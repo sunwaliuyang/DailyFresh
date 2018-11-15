9bac61a6ebb36b429c78ec6fe2ab76a83288a1b7
liuyang <liuyang1283@dingtalk.com>
2018-11-15 10:52:01 +0800

    注册请求，使用类视图功能

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 from django.urls import include, path
-from . import views
+# from . import views
+from user.views import RegisterView
 
 app_name = 'user'
 urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('register_submit', views.register_submit, name='register_submit'),
+    path('register/', RegisterView.as_view(), name='register'),
+    # path('register/', views.register, name='register'),
+    # path('register_submit', views.register_submit, name='register_submit'),
 ]
--- apps/user/views.py
+++ apps/user/views.py
@@ -1,19 +1,51 @@
 from django.shortcuts import render , redirect
 import re
 from user.models import MyUser,Address
+# from django.http import HttpResponse
 from django.urls import reverse
+from django.views.generic import View
 from django.middleware.csrf import CsrfViewMiddleware
 # Create your views here.
-
+# 使用内置视图 
 def register(request):
     """注册页"""
-    return render(request, "user/register.html")
-    # if request.method == "GET":  # 如果请求方式是GET请求 代表请求注册页面
-        # context["user"] = None  # 初始化模板user参数为None
-        # context["captcha"] = forms.Check_Code()  # 传递验证码 直接返回模板
+    #第一种写法
+    if request.method == 'GET':
+        return render(request, "user/register.html")
+    elif request.method == 'POST':
+        username = request.POST.get('user_name')
+        password = request.POST.get('pwd')
+        email = request.POST.get('email')
+        allow = request.POST.get('allow')
+        # 验证
+        if not all([username, password, email]):
+            return render(request, "user/register.html", {'errmsg': '数据不完整'})
+        if re.match('/^[a-z0-9][\w.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$/', email):
+            return render(request, "user/register.html", {'errmsg': '邮箱格式 不正确'})
+        if allow != 'on':
+            return render(request, "user/register.html", {'errmsg': '请同意用户协议'})
+        # 检测用户名信息 是否存在
+        try:
+            user = MyUser.objects.get(username=username)
+        except MyUser.DoesNotExist:
+            user = None
+        # 业务处理
+        '''第一种创建方式'''
+        # user = MyUser()
+        # user.username = username
+        # user.password = password
+        # user.email = email
+        # user.save()
+
+        if user:
+            return render(request, "user/register.html", {'errmsg': '用户名已经存在'})
+        else:
+            user = MyUser.objects.create_user(username, email, password)
+        if user == True:
+            pass
         # return render(request, "user/register.html")
-    # elif request.method == "POST":
-    #     return render(request, "user/login.html")
+        return redirect(reverse('goods:index'))
+    #使用类视图
 def register_submit(request):
     '''接受数据'''
     username = request.POST.get('user_name')
@@ -27,6 +59,11 @@ def register_submit(request):
         return render(request, "user/register.html", {'errmsg': '邮箱格式 不正确'})
     if allow != 'on':
         return render(request, "user/register.html", {'errmsg': '请同意用户协议'})
+    #检测用户名信息 是否存在
+    try:
+        user = MyUser.objects.get(username=username)
+    except MyUser.DoesNotExist:
+        user = None
     #业务处理
     '''第一种创建方式'''
     # user = MyUser()
@@ -34,9 +71,51 @@ def register_submit(request):
     # user.password = password
     # user.email = email
     # user.save()
-    user = MyUser.objects.create_user(username,email,password)
+
+    if  user:
+        return render(request, "user/register.html", {'errmsg': '用户名已经存在'})
+    else:
+        user = MyUser.objects.create_user(username, email, password)
     if user == True:
         pass
     # return render(request, "user/register.html")
     return redirect(reverse('goods:index'))
     #返回应答
+
+'''类视图'''
+class RegisterView(View):
+    def get(self,request):
+        return render(request, "user/register.html")
+    def post(self,request):
+        username = request.POST.get('user_name')
+        password = request.POST.get('pwd')
+        email = request.POST.get('email')
+        allow = request.POST.get('allow')
+        # 验证
+        if not all([username, password, email]):
+            return render(request, "user/register.html", {'errmsg': '数据不完整'})
+        if re.match('/^[a-z0-9][\w.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$/', email):
+            return render(request, "user/register.html", {'errmsg': '邮箱格式 不正确'})
+        if allow != 'on':
+            return render(request, "user/register.html", {'errmsg': '请同意用户协议'})
+        # 检测用户名信息 是否存在
+        try:
+            user = MyUser.objects.get(username=username)
+        except MyUser.DoesNotExist:
+            user = None
+        # 业务处理
+        '''第一种创建方式'''
+        # user = MyUser()
+        # user.username = username
+        # user.password = password
+        # user.email = email
+        # user.save()
+
+        if user:
+            return render(request, "user/register.html", {'errmsg': '用户名已经存在'})
+        else:
+            user = MyUser.objects.create_user(username, email, password)
+        if user == True:
+            pass
+        # return render(request, "user/register.html")
+        return redirect(reverse('goods:index'))
