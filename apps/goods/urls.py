8100fba0ba0a635b240728ac35a0d8fcdcaeb41c
liuyang <liuyang1283@dingtalk.com>
2018-11-29 15:55:31 +0800

    fdfs文件上传系统部署 商品首页，详情页功能

--- apps/goods/urls.py
+++ apps/goods/urls.py
@@ -1,9 +1,12 @@
 
 from django.conf.urls import url
 from django.urls import include, path
-from . import views
+from goods.views import IndexView,ListView,DetailView
 
 app_name = 'goods'
 urlpatterns = [
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
+    path('', IndexView.as_view(), name='index'),
+    path('goods/<int:goods_id>', DetailView.as_view(), name='detail'),
+    path('list/<int:type_id>/<int:page>', ListView.as_view(), name='list'),
 ]
--- apps/order/urls.py
+++ apps/order/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 from django.urls import include, path
-from . import views
+# from . import views
+from order.views import OrderPlaceView, OrderCommitView
 
 app_name = 'order'
 urlpatterns = [
+    path('place/', OrderPlaceView.as_view(), name='place'),
+    path('commit/', OrderCommitView.as_view(), name='commit'),
 ]
